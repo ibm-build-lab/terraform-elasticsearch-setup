cc6be638fad31364a6b4580f22927dcf08db9f07
Ann Umberhocker <ann.umberhocker@ibm.com>
2024-10-21 10:23:03 -0500

    fixing resource to add host_flavor

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,12 +3,17 @@ variable "region" {
     default = "us-south"
 }
 variable "es_database_name" {
-    default = "elastic_db"
+    default = "ma_elastic_db"
 }
 
 variable "es_password" {
     description = "Password must have between 15 and 32 characters, must contain a number, can include A-Z, a-z, 0-9, -, _, but can not start with special characters."
 }
+
+variable "es_host_flavor" {
+    default = "b3c.4x16.encrypted"
+}
+
 variable "es_version" {
     default = "8.12"
 }
@@ -22,11 +27,11 @@ variable "es_cpu_count" {
     default = 3
 }
 variable "es_resource_group" {
-    default = "elasticsearch_rg"
+    default = "ma_elasticsearch_rg"
 }
 variable "es_tags" {
     default = ["env:dev"]
 }
 variable "ce_project" {
-    default = "elasticsearch_proj"
+    default = "ma_elasticsearch_proj"
 }
